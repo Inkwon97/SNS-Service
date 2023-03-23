1cfd2f93f71e20afee0346f049083ae786c88d86
ingwon97 <ghkdrnjs180@naver.com>
2023-03-23 11:02:35 +0900

    #1 - webStomp로 채팅 서버 구현

---
+++ src/main/java/com/project/snsservice/chat/WebSocketConfig.java
@@ -0,0 +1,7 @@
+package com.project.snsservice.chat;
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@ConfigurationPropertiesScan
+public class WebSocketConfig {
+}
---
+++ src/main/java/com/project/snsservice/chat/config/EnbeddedRedisConfig.java
@@ -0,0 +1,11 @@
+package com.project.snsservice.chat.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("local")
+@Configuration
+public class EnbeddedRedisConfig {
+
+}
