58515d7ff8f9fae83c5037f70808f49586707d37
ingwon97 <ghkdrnjs180@naver.com>
2023-04-08 03:49:21 +0900

    폴더 구조 변경

--- src/main/java/com/project/snsservice/config/ObjectMapperConfig.java
+++ src/main/java/com/project/snsservice/config/ObjectMapperConfig.java
@@ -1,4 +1,4 @@
-package com.project.snsservice.global.config;
+package com.project.snsservice.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
--- src/test/java/com/project/redis/RedisTemplateTest.java
+++ src/test/java/com/project/redis/RedisTemplateTest.java
@@ -1,14 +1,9 @@
 package com.project.redis;
 
 import com.project.AbstractIntegrationContainerBaseTest;
-import com.project.snsservice.global.config.RedisConfig;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.project.snsservice.config.RedisConfig;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.redis.core.HashOperations;
-import org.springframework.data.redis.core.RedisTemplate;
 
 @Import(RedisConfig.class)
 @SpringBootConfiguration
